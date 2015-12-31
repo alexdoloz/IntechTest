ccfe33afd498e8b90265c5665285db03b5802a43
Alexander Doloz <doloz.alexander@gmail.com>
2015-12-31 17:07:30 +0300

    Initial Commit

---
+++ IntechTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IntechTest
+//
+//  Created by Alexander on 31.12.15.
+//  Copyright © 2015 Alexander. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ IntechTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IntechTest
+//
+//  Created by Alexander on 31.12.15.
+//  Copyright © 2015 Alexander. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
